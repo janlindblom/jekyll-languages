524b59d652c62affd0b883732c946b45cbe64f45
Jan Lindblom <lindblom.jan@gmail.com>
2014-03-14 22:54:07 +0100

    Specs fixed, should run on any ruby now.

--- spec/languages_spec.rb
+++ spec/languages_spec.rb
@@ -5,6 +5,6 @@ require 'jekyll'
 
 describe Jekyll::Languages do
   it "can sentencize an array into a readable sentence form" do
-    Liquid::Template.parse("{{ array | sentencize_array }}").render( 'array' => ['äpplen', 'päron', 'tomater'] ).should match /(.*), (.*) and (.*)/
+    Liquid::Template.parse("{{ array | sentencize_array }}").render( 'array' => ['apples', 'bananas', 'cucumbers'] ).should match /(.*), (.*) and (.*)/
   end
 end
